7495d62a0f1deca011bfb797cbae8dcff1b4db7d
01xz <73350430+01xz@users.noreply.github.com>
2023-03-16 16:48:22 +0800

    chore: remove print()

--- esbmf.py
+++ esbmf.py
@@ -54,13 +54,8 @@ def boolean_matrix_error_minimize(orig: np.ndarray, approx: np.ndarray, dc_row: 
     rows = orig.shape[0]
     diff = lambda i: hamming_distance(approx[i, :], orig[i, :]) - hamming_distance((approx | dc_row)[i, :], orig[i, :])
     diff_col = np.vectorize(diff)(np.arange(rows))
-    print("diff_col:")
-    print(diff_col)
     num_p, num_n = np.count_nonzero(diff_col > 0), np.count_nonzero(diff_col < 0)
-    # print(f"p: {num_p}, n: {num_n}")
     replace_zeros = np.where(diff_col == 0, int(num_p > num_n), diff_col)
-    # print("replace zeros:")
-    # print(replace_zeros)
     c_col = np.where(replace_zeros < 0, 0, replace_zeros).astype(bool)
     return c_col
 
@@ -83,7 +78,5 @@ def boolean_matrix_column_error_clear(orig: np.ndarray, approx: np.ndarray, c_co
     diff = lambda i: hamming_distance(approx[:, i], orig[:, i]) - hamming_distance(
         (approx.transpose() | c_col).transpose()[:, i], orig[:, i])
     diff_row = np.vectorize(diff)(np.arange(cols))
-    print("diff_row:")
-    print(diff_row)
     c_col = np.where(diff_row < 0, 0, diff_row).astype(bool)
     return c_col
